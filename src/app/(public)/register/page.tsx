f99354e4227cee14184dedd8e3a79ff8cbd022b2
nurulamin092 <mnurulamin092@gmail.com>
2026-02-14 21:10:45 +0600

    refactor  move auth to feature-based structure

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from "@/components/modules/authentication/register-form";
+import { RegisterForm } from "@/features/auth/components/register-form";
 
 export default function Page() {
   return (
