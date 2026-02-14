82e87081f746304a0bbb61b312c1aac11a8131fa
nurulamin092 <mnurulamin092@gmail.com>
2026-02-14 17:09:18 +0600

    google  login

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
-export default function Home() {
+export default async function Home() {
+  const session = await authClient.getSession();
+  console.log(session);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Button variant={"outline"}>Click me</Button>
