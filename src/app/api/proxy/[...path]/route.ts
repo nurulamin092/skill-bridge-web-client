050133ffca36486431f5157eaba638e08f400f9c
nurulamin092 <mnurulamin092@gmail.com>
2026-03-11 13:06:57 +0600

    fix handle auth and api routes separately in proxy

--- src/app/api/proxy/[...path]/route.ts
+++ src/app/api/proxy/[...path]/route.ts
@@ -141,8 +141,48 @@ import { NextRequest, NextResponse } from "next/server";
 
 const BACKEND = env.BACKEND_URL || "http://localhost:5000";
 
+// async function proxy(req: NextRequest, path: string[]) {
+//   // const url = `${BACKEND}/api/v1/${path.join("/")}${req.nextUrl.search}`;
+//   let url: string;
+
+//   if (path[0] === "auth") {
+//     url = `${BACKEND}/api/${path.join("/")}${req.nextUrl.search}`;
+//   } else {
+//     url = `${BACKEND}/api/v1/${path.join("/")}${req.nextUrl.search}`;
+//   }
+
+//   const originHeader = req.headers.get("origin") || env.NEXT_PUBLIC_APP_URL;
+
+//   const res = await fetch(url, {
+//     method: req.method,
+//     headers: {
+//       cookie: req.headers.get("cookie") || "",
+//       "content-type": req.headers.get("content-type") || "application/json",
+//       origin: originHeader,
+//     },
+//     body: req.method !== "GET" ? await req.text() : undefined,
+//     credentials: "include",
+//   });
+
+//   const response = new NextResponse(res.body, {
+//     status: res.status,
+//   });
+
+//   const setCookie = res.headers.get("set-cookie");
+//   if (setCookie) {
+//     response.headers.set("set-cookie", setCookie);
+//   }
+
+//   return response;
+// }
 async function proxy(req: NextRequest, path: string[]) {
-  const url = `${BACKEND}/api/v1/${path.join("/")}${req.nextUrl.search}`;
+  let url: string;
+
+  if (path[0] === "auth") {
+    url = `${BACKEND}/api/${path.join("/")}${req.nextUrl.search}`;
+  } else {
+    url = `${BACKEND}/api/v1/${path.join("/")}${req.nextUrl.search}`;
+  }
 
   const originHeader = req.headers.get("origin") || env.NEXT_PUBLIC_APP_URL;
 
@@ -168,7 +208,6 @@ async function proxy(req: NextRequest, path: string[]) {
 
   return response;
 }
-
 export async function GET(
   req: NextRequest,
   context: { params: Promise<{ path: string[] }> },
